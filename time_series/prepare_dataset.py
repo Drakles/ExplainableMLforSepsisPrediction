98af2f8b0134327b690e245ab349731f2379feeb
Kajetan <drakles1@gmail.com>
2021-08-05 19:11:56 +0100

    series dataset improvements

--- time_series/prepare_dataset.py
+++ time_series/prepare_dataset.py
@@ -20,31 +20,41 @@ def summary_missing_val(dataframe):
 
 def prepare_time_series_dataset(df, selected_features):
     # filter for selected columns
-    df = df[selected_features]
+    df = df[['PatientID'] + selected_features + ['Day', 'OrdinalHour']]
+
+    df['TimeStamp'] = df.Day.astype(str).str.cat(df.OrdinalHour.astype(str),
+                                                 sep='-')
+    df = df.drop(['Day', 'OrdinalHour'], axis=1)
 
     patientIDs = set(df['PatientID'])
 
     for patientID in patientIDs:
-        df.loc[df['PatientID'] == patientID, selected_features[1:]] = \
-            df[df['PatientID'] == patientID][selected_features[1:]] \
-                .interpolate(method='pad', limit_direction='forward') \
-                .interpolate(method='bfill')
+        # single patients cannot have more than one same timestamp
+        if df[df['PatientID'] == patientID]['TimeStamp'].is_unique:
+            df.loc[df['PatientID'] == patientID, selected_features] = \
+                df[df['PatientID'] == patientID][selected_features] \
+                    .interpolate(method='pad', limit_direction='forward') \
+                    .interpolate(method='bfill') \
+                    .tail(24)
+        else:
+            df = df.drop(df[df['PatientID'] == patientID].index)
+
+    df = df.drop('TimeStamp', axis=1)
 
     df = knn_imputing(df)
 
     patient_id_series = {}
 
     for id, patient_id_df in df.groupby('PatientID'):
-        patient_id_series[id] = [id] + [patient_id_df[col_series]
-                                            .tail(24)
+        patient_id_series[id] = [id] + [patient_id_df[sel_col]
                                             .reset_index(drop=True)
-                                        for col_series in selected_features[1:]]
+                                        for sel_col in selected_features]
 
-    resultd_df = pd.DataFrame. \
+    result_df = pd.DataFrame. \
         from_dict(patient_id_series, "index", columns=df.columns) \
         .set_index('PatientID')
 
-    return resultd_df
+    return result_df
 
 
 def knn_imputing(df):
@@ -55,31 +65,31 @@ def knn_imputing(df):
     return df
 
 
-if __name__ == '__main__':
-    df_series_non_sepsis = pd.read_csv('../data/FinalNonSepsisSeries.csv')
-    df_series_sepsis = pd.read_csv('../data/FinalSepsisSeries.csv')
-
-    columns_to_drop = ['PatientID', 'Day', 'OrdinalHour', 'Mortality14Days',
-                       'comorbidity', 'Admit Ht']
-
-    # shared features
+def save_df_interesetion_features(df_series_sepsis, df_series_non_sepsis,
+                                  columns_to_drop):
     columns = sorted(list(set(df_series_sepsis.columns.values)
         .intersection(
         set(df_series_non_sepsis.columns.values))))
     columns = [col for col in columns if col not in columns_to_drop]
-    columns.insert(0, 'PatientID')
-
     series_non_sepsis_df = prepare_time_series_dataset(df_series_non_sepsis,
                                                        columns)
-
     series_sepsis_df = prepare_time_series_dataset(df_series_sepsis,
                                                    columns)
-
-    # exclude patients from non sepsis if they are in sepsis file
-    series_non_sepsis_df = series_non_sepsis_df[
-        ~series_non_sepsis_df.index.isin(series_sepsis_df.index.values)]
 
     series_non_sepsis_df.to_pickle(
         '../data/preprocessed_data/series_non_sepsis.pkl')
     series_sepsis_df.to_pickle('../data/preprocessed_data/series_sepsis.pkl')
 
+
+if __name__ == '__main__':
+    df_series_non_sepsis = pd.read_csv('../data/FinalNonSepsisSeries.csv')
+    df_series_sepsis = pd.read_csv('../data/FinalSepsisSeries.csv')
+
+    columns_to_drop = ['PatientID', 'Mortality14Days', 'Day', 'OrdinalHour',
+                       'comorbidity', 'Admit Ht', 'Admission Weight (Kg)',
+                       'Admission Weight (lbs.)', 'Height (cm)', 'Height',
+                       'Hour', 'weight', 'Age']
+
+    # use only shared features
+    save_df_interesetion_features(df_series_sepsis, df_series_non_sepsis,
+                                  columns_to_drop)
